81d33eb0b7d89a3ca5cb2825643c4433cd120bc3
Anshuman Khaund <akhaund@users.noreply.github.com>
2020-11-11 14:15:54 -0500

    Update mca.py

--- mca.py
+++ mca.py
@@ -9,6 +9,7 @@
 import numpy as np
 import pandas as pd
 
+
 from scipy.linalg import svd
 
 import plots
@@ -17,41 +18,47 @@ import plots
 class OutputMCA:
     """ Multiple Correspondence Analysis (under construction) """
 
-    def __init__(self) -> None:
+    def __init__(self, df) -> None:
+        self._df = df
         pass
 
-    def do_mca(df):
+    def check_sparcity(self):
+        is_sparse = False
+        return is_sparse
+
+    def do_mca(self):
         """"""
-        x = df.values
-        N = np.sum(x)
-        Z = x / N
+        # x = df.values
+        # N = np.sum(x).sum()
+        # Z = x / N
 
-        sum_r = np.sum(Z, axis=1)
-        sum_c = np.sum(Z, axis=0)
+        # sum_r = np.sum(Z, axis=1)
+        # sum_c = np.sum(Z, axis=0)
 
-        Z_expected = np.outer(sum_r, sum_c)
-        Z_residual = Z - Z_expected
+        # Z_expected = np.outer(sum_r, sum_c)
+        # Z_residual = Z - Z_expected
 
-        D_r_sqrt = np.sqrt(np.diag(sum_r ** -1))
-        D_c_sqrt = np.sqrt(np.diag(sum_c ** -1))
+        # D_r_sqrt = np.sqrt(np.diag(sum_r ** -1))
+        # D_c_sqrt = np.sqrt(np.diag(sum_c ** -1))
 
-        mca_mat = D_r_sqrt @ Z_residual @ D_c_sqrt
-        _, S, Qh = svd(mca_mat, full_matrices=False)
-        Q = Qh.T
+        # mca_mat = D_r_sqrt @ Z_residual @ D_c_sqrt
+        # _, S, Qh = svd(mca_mat, full_matrices=False)
+        # Q = Qh.T
 
-        G = D_c_sqrt @ Q @ np.diag(S)
+        # G = D_c_sqrt @ Q @ np.diag(S)
 
-        eig_vals = S ** 2
-        expl_var_ratio = eig_vals / eig_vals.sum()
-        expl_var = pd.DataFrame({
-            "var_exp": expl_var_ratio,
-            "cumul_var_exp": expl_var_ratio.cumsum()})
+        # eig_vals = S ** 2
+        # expl_var_ratio = eig_vals / eig_vals.sum()
+        # expl_var = pd.DataFrame({
+        #     "var_exp": expl_var_ratio,
+        #     "cumul_var_exp": expl_var_ratio.cumsum()})
 
-        # Plotting
-        fig = plots.explained_variance_plot(expl_var)
-        return fig, G
+        # # Plotting
+        # fig = plots.explained_variance_plot(expl_var)
+        return
 
 
 # Test
 if __name__ == "__main__":
-    pass
+    df = pd.read_csv("data/burgundies.csv",
+                     skiprows=1, index_col=0, header=0)
