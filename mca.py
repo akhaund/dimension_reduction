07770d9b856aab8db916233e93834ebf6a0a4e6e
Anshuman Khaund <akhaund@users.noreply.github.com>
2020-11-15 21:49:57 -0500

    Update mca.py
    Multiple edits.

--- mca.py
+++ mca.py
@@ -19,6 +19,8 @@ from collections import namedtuple
 
 import plots
 
+import plotly.express as px
+
 
 class OutputMCA:
     """ Multiple Correspondence Analysis (under construction) """
@@ -26,7 +28,9 @@ class OutputMCA:
     def __init__(self, df) -> None:
         if self._check_sparcity():
             self._sparse_type = sp.csr_matrix
-            self._dat = self._sparse_type(df).values
+            self._dat = self._sparse_type(df.values)
+            self._columns = df.columns
+            self._index = df.index
             self._sum = self._sparse_type.sum
             self._diag = sp.diags
             self._svd = spl.svds
@@ -34,6 +38,8 @@ class OutputMCA:
             self._inv = spl.inv
         else:
             self._dat = df.values
+            self._columns = df.columns
+            self._index = df.index
             self._sum = np.sum
             self._diag = np.diag
             self._svd = sl.svd
@@ -70,14 +76,18 @@ class OutputMCA:
         weight_row = np.sqrt(inv(diag(sum_row)))
         weight_col = np.sqrt(inv(diag(sum_col)))
         Z_weighted = weight_row @ Z_centered @ weight_col
-        P, S, Qh = svd(Z_weighted, full_matrices=False)
-        Q = Qh.T
+        P, S, Qh = svd(Z_weighted, full_matrices=False)  # Qh are rows
         F = weight_row @ P @ diag(S)  # row factors
-        G = weight_col @ Q @ diag(S)  # column factors
-
+        G = weight_col @ Qh.T @ diag(S)  # column factors
         return mca_out(S ** 2, F, G)
 
-    def explained_variance_plot(self):
+    def _benzecri_correction():
+        return
+
+    def _greenacre_correction():
+        return
+
+    def get_explained_variance(self):
         """ Pareto chart of explained variance
         """
         inertia = getattr(self._mca, "inertia")
@@ -87,8 +97,6 @@ class OutputMCA:
             "cumul_var_exp": expl_var_ratio.cumsum()
         })
         expl_var.index += 1
-
-        # Plotting
         fig = plots.explained_variance_plot(
             df=expl_var,
             x_title="Latent Variable Rank",
@@ -97,7 +105,7 @@ class OutputMCA:
         )
         return fig
 
-    def scree_plot(self):
+    def get_scree_plot(self):
         eig_vals = pd.DataFrame({
             "eig_val": getattr(self._mca, "inertia")
         })
@@ -110,8 +118,11 @@ class OutputMCA:
         )
         return fig
 
-    def get_projections():
-        return
+    def get_projections(self):
+        G = getattr(self._mca, "latent_rows")
+
+        A = pd.DataFrame(G[:, :2], columns=["A", "B"])
+        px.scatter(A, x="A", y="B").show()
 
     def get_components():
         return
@@ -121,6 +132,23 @@ class OutputMCA:
 if __name__ == "__main__":
     df = pd.read_csv("data/burgundies.csv",
                      skiprows=1, index_col=0, header=0)
+    target = df["oak_type"]
+    df.drop(columns="oak_type", inplace=True)
 
-    OutputMCA(df).explained_variance_plot()
-    OutputMCA(df).scree_plot()
+    experts = ['Expert_1'] * 7 + ['Expert_2'] * 9 + ['Expert_3'] * 6
+    feature = (['fruity'] * 2 + ['woody'] * 3 + ['coffee'] * 2 +
+               ['fruity'] * 2 + ['roasted'] * 2 + ['vanilla'] * 3 +
+               ['woody'] * 2 + ['fruity'] * 2 + ['butter'] * 2 +
+               ['woody'] * 2)
+    binary = list('yn')
+    ternary = list('123')
+    observations = binary + ternary + binary * 3 + ternary + binary * 4
+    column_index = pd.MultiIndex.from_arrays([experts, feature, observations])
+    df.columns = column_index
+
+    # Explained variance
+    OutputMCA(df).get_explained_variance().show()
+    # Scree plot
+    OutputMCA(df).get_scree_plot().show()
+    # Get prokections
+    OutputMCA(df).get_projections()
